36cca70b33bce93a7b2517f9867cbc8f8b79dd41
rira199412 <rira2.0@mail.ru>
2023-06-06 00:09:00 +0300

    ДЗ по БазовымАлгоритмам

---
+++ BazAgoritm/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Console.Write("Введи трёхзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string ndex = Convert.ToString(N);
+Console.WriteLine("вторая цифра этого числа = "+ndex[1]);
+*/
+/*
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string ndex = Convert.ToString(N);
+ if (ndex.Length >2)    
+{
+    Console.Write("третья цифра этого числа = "+ndex[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
+*/
+/*
+Console.Write("Введите день недели: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N >= 1 && N <= 5)
+{
+    Console.WriteLine("НЕТ");
+}
+else if (N >=6 && N <= 7)
+{
+     Console.WriteLine("ДА");
+}
+else 
+{
+    Console.WriteLine("НЕТ ТАКОГО ДНЯ НЕДЕЛИ");
+}
+*/
+
